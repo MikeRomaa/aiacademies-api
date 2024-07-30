47cec791dc09af8690d0c939950a170297b228a0
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-29 22:38:58 -0400

    Update admin.py

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from .models import Feedback
 
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'feedback', 'email', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('feedback', 'email')
+    ordering = ('-created_at',)  # Orders by creation date, newest first
