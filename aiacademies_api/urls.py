37e73ce7d460975a9f5abec3c62589fc6d671738
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-29 22:42:50 -0400

    Update urls.py

--- aiacademies_api/urls.py
+++ aiacademies_api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('auth/', include('apps.oauth.urls')),
     path('api/', include('apps.courses.urls')),
     path('api/', include('apps.blog.urls')),
+    path('api/', include('apps.feedback.urls')),
     path('admin/', admin.site.urls),
 ]
