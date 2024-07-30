3af427997dc96a397862572ce97ef4dabef078d0
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-07-29 22:47:17 -0400

    Update views.py

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_thanks')  # Redirect to a thank-you page or similar
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
