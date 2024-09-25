3f5076b29517412e1b543eec8287af870c116da6
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2024-09-24 22:26:18 -0400

    Update urls.py

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('courses/', views.CourseListCreateView.as_view(), name='course-list'),
-    path('courses/<int:course_id>/', views.CourseInstanceView.as_view(), name='course-detail'),
-    path('lessons/<int:lesson_id>/', views.LessonInstanceView.as_view(), name='lesson-detail'),
-    path('quizzes/<int:quiz_id>/', views.QuizInstanceView.as_view(), name='quiz-detail'),
-    path('quizzes/<int:quiz_id>/review/', views.ReviewQuizAttemptView.as_view(), name='quiz-review'),
+    path('courses/', views.CourseListCreateView.as_view()),
+    path('courses/<int:course_id>/', views.CourseInstanceView.as_view()),
+    path('lessons/<int:lesson_id>/', views.LessonInstanceView.as_view()),
+    path('quizzes/<int:quiz_id>/', views.QuizInstanceView.as_view()),
+    path('quizzes/<int:quiz_id>/review/', views.ReviewQuizAttemptView.as_view()),
 ]
